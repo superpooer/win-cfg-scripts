9d7a6e78088509d70a16feee6e6ebd986f7a983c
superpooer <teh.epik.dukk@gmail.com>
2020-06-29 11:58:30 +0100

    reorder and remove garbage

--- todo.bat
+++ todo.bat
@@ -3,12 +3,9 @@ cls
 echo -------------------------------------------
 echo I                                         I
 echo I    TODO:                                I
+echo I    calculator.c                         I
 echo I    HUD                                  I
 echo I    CONTINUE book manager                I
-echo I    cmdgfx frog shooting BLANK rp        I
 echo I    temple on spare hdd/thiccpad         I
-echo I    learn latex by answerin q217         I
-echo I    make asmblr/disasm                   I
-echo I    calculator.c                         I
 echo I                                         I
 echo -------------------------------------------
