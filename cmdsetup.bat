199ac5ddf1b088698b11680921025d7cf05f3e09
superpooer <teh.epik.dukk@gmail.com>
2020-09-18 20:17:45 +0100

    updated todo

--- cmdsetup.bat
+++ cmdsetup.bat
@@ -6,7 +6,7 @@ doskey pox=popdx $*
 doskey norm_cd=cd $*
 color 02
 cd C:\Users\tehep
-PATH=C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\Users\tehep\games;C:\Users\tehep\tools;C:\Program Files\yt-dl;;C:\Program Files\ffmpeg\bin;C:\Program Files\nircmd;C:\Program Files\Neovim\bin;C:\Program Files\7-zip;C:\Program Files\mpv;C:\Program Files\MPC-HC;C:\Program Files\tcc;C:\Program Files\Git\bin;C:\Program Files\SumatraPDF;C:\Program Files\TortoiseSVN\bin;C:\Program Files\fasm;C:\Program Files\homebrew;C:\MinGW\msys\1.0\bin;
+PATH=C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\Users\tehep\games;C:\Users\tehep\tools;C:\Program Files\yt-dl;;C:\Program Files\ffmpeg\bin;C:\Program Files\nircmd;C:\Program Files\Neovim\bin;C:\Program Files\7-zip;C:\Program Files\mpv;C:\Program Files\MPC-HC;C:\Program Files\tcc;C:\Program Files\Git\bin;C:\Program Files\SumatraPDF;C:\Program Files\TortoiseSVN\bin;C:\Program Files\fasm;C:\Program Files\homebrew;C:\MinGW\msys\1.0\bin;C:\texlive\2020\bin\win32\;
 set ME=%USERPROFILE%
 set PF="C:\Program Files"
 set X86="C:\Program Files (x86)"
@@ -18,6 +18,8 @@ set REPOS=%USERPROFILE%\repos
 set HB="C:\Program Files\homebrew"
 set MU=%USERPROFILE%\Music
 set TFCFG="C:\Program Files\Steam\steamapps\common\Team Fortress 2\tf\cfg"
+set STARTUP="C:\Users\tehep\Appdata\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+set TASKBAR="C:\Users\tehep\Appdata\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\Taskbar"
 title cmd
 prompt tehep[$+]$S
 ::^^hardcoded prompt but we cd here so itll never not be true
@@ -33,8 +35,9 @@ doskey sdn=shutdown /p
 doskey sdrn=shutdown /r /t 0
 doskey yt=youtube-dl -i --geo-bypass --add-metadata --console-title -o "C:\\Users\\tehep\\Videos\\yt-dl\\%%(title)s-(%%(channel)s)-%%(id)s.%%(ext)s" $1
 doskey ytfile=youtube-dl -i --geo-bypass --add-metadata --console-title -o "C:\\Users\\tehep\\Videos\\yt-dl\\%%(title)s-(%%(channel)s)-%%(id)s.%%(ext)s" -a C:\Users\tehep\to_dl.bat
+doskey ytpl=youtube-dl -i --geo-bypass --add-metadata --console-title -o "C:\\Users\\tehep\\Videos\\$1\\%%(title)s-(%%(channel)s)-%%(id)s.%%(ext)s" $2
 doskey yta=youtube-dl -i -x --geo-bypass --add-metadata --console-title -o "C:\\Users\\tehep\\Music\\yt-dl\\%%(title)s-(%%(channel)s)-%%(id)s.%%(ext)s" $1
-doskey ytalbum=youtube-dl -i -x --geo-bypass --add-metadata --console-title -o "C:\\Users\\tehep\\Music\\yt-dl\\$1\\%%(title)s-(%%(channel)s)-%%(id)s.%%(ext)s" $2
+doskey ytalbum=youtube-dl -i -x --geo-bypass --add-metadata --console-title -o "C:\\Users\\tehep\\Music\\$1\\%%(title)s-(%%(channel)s)-%%(id)s.%%(ext)s" $2
 doskey appdata=cd C:\Users\tehep\AppData
 doskey c=cdx C:\
 doskey vrc=v C:\Users\tehep\Appdata\Local\nvim\init.vim
--- todo.bat
+++ todo.bat
@@ -3,9 +3,8 @@ cls
 echo -------------------------------------------
 echo I                                         I
 echo I    TODO:                                I
-echo I    calculator.c                         I
 echo I    HUD                                  I
-echo I    CONTINUE book manager                I
 echo I    temple on spare hdd/thiccpad         I
+echo I    learn enums n make ger plate thing   I
 echo I                                         I
 echo -------------------------------------------
