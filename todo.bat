630d50e598a09baac0fe8a4b1035c447359d8b75
superpooer <teh.epik.dukk@gmail.com>
2020-10-25 15:26:36 +0000

    i do temple now))

--- todo.bat
+++ todo.bat
@@ -3,9 +3,7 @@ cls
 echo -------------------------------------------
 echo I                                         I
 echo I    TODO:                                I
-echo I    HUD                                  I
-echo I    temple on spare hdd/thiccpad         I
-echo I    make ger plate thing - site dld      I
+echo I    nothing :)                           I
 echo I                                         I
 echo -------------------------------------------
 
