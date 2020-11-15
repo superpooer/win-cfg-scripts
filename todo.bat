85effb0b3e8467097bec498c30b2f97d4baa46eb
superpooer <NicoTAKulmann@gmail.com>
2020-11-15 01:21:01 +0000

    random bool networks

--- todo.bat
+++ todo.bat
@@ -4,6 +4,7 @@ echo -------------------------------------------
 echo I                                         I
 echo I    TODO:                                I
 echo I    sort vids w door                     I
+echo I    random bool network                  I
 echo I                                         I
 echo -------------------------------------------
 
