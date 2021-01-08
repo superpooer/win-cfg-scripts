ba7423d1bf2084237098648d2cdfd1c2389cdfda
superpooer <teh.epik.dukk@gmail.com>
2021-01-08 12:25:52 +0000

    dpu

--- todo.bat
+++ todo.bat
@@ -5,6 +5,7 @@ echo I                                         I
 echo I    TODO:                                I
 echo I    sort vids w door                     I
 echo I    hash coll detector                   I
+echo I    dpu - start w 1c                     I
 echo I                                         I
 echo -------------------------------------------
 
