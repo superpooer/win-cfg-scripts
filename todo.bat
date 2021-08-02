0ce5ee753ef61930eafbc5298c17a672c4a3c870
superpooer <teh.epik.dukk@gmail.com>
2021-08-02 20:09:59 +0100

    fucking why does the start menu take 20 seconds to open on a system thats been up for 4 hours and when typing just sits there like a mong

--- todo.bat
+++ todo.bat
@@ -3,6 +3,7 @@ cls
 echo -------------------------------------------
 echo I                                         I
 echo I    TODO:                                I
+echo I    fix lucas pc                         I
 echo I                                         I
 echo -------------------------------------------
 
